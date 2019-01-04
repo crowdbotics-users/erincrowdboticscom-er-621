ed6a8a42bdb3017e9978a63d26d112e5a4b7156f
Saurav <sp.gharti@gmail.com>
2019-01-04 14:46:22 +0000

    add package twilio

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'twilio', 'url': 'http://pypi.python.org/pypi/twilio/6.10.0'},
 	{'name':'django-directory', 'url': 'http://pypi.python.org/pypi/django-directory/0.0.1'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
 	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
