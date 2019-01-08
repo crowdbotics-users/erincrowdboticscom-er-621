21eb9a27b847bdc460b39fda284fd054ed5888e0
Saurav <sp.gharti@gmail.com>
2019-01-08 20:28:19 +0000

    add package google-assistant-sdk

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'google-assistant-sdk', 'url': 'http://pypi.python.org/pypi/google-assistant-sdk/0.4.2'},
 	{'name':'django-directory', 'url': 'http://pypi.python.org/pypi/django-directory/0.0.1'},
 	{'name':'djangorestframework', 'url': 'http://pypi.python.org/pypi/djangorestframework/3.7.7'},
 	{'name':'django-bootstrap4', 'url': 'http://pypi.python.org/pypi/django-bootstrap4/0.0.5'},
